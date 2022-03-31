ad40997135db69c1f8d8f5949396ed95bc36039f
jad-rodriguez <jaylordrodriguez@gmail.com>
2022-04-01 10:51:27 +1300

    create addressbook view, initial layout template

---
+++ resources/views/addressbook/layout.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <title>Address Book Application</title>
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.6.1/css/bootstrap.css" rel="stylesheet">
+    </head>
+    <body>
+        <h1>Address Book Application</h1>
+        <div class="container">
+            <br>
+            @yield('content')
+        </div>
+    </body>
+</html>
